1c55d6cbb19b2bd415056ae9e893f9994abf89e2
Steve Conover <steve.conover@soundcloud.com>
2018-05-04 17:47:31 +0200

    still hacking. made a chord diagram

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const d3 = require("d3")
 
-console.log(Object.keys(d3))
-
 const zipkinBaseUrl = process.env.ZIPKIN_BASE_URL
 
 console.log("hello world 7", zipkinBaseUrl)
@@ -29,118 +27,136 @@ function zipkinDepsToD3Graph(zipkinDependencyResponse) {
   }
 }
 
-function buildVisualization(d3GraphData) {
-  var svg = d3.select("svg");
+function zipkinDepsToD3Matrix(zipkinDependencyResponse) {
+  const nodeSet = {}
+  zipkinDependencyResponse.forEach(parentChildRow => {
+    nodeSet[parentChildRow.parent] = true
+    nodeSet[parentChildRow.child] = true
+  })
+  const nodeNames = Object.keys(nodeSet)
+  nodeNames.sort()
 
-  var width = svg.attr("width");
-  var height = svg.attr("height");
+  const nameToIndex = {}
+  const indexToName = {}
+  for (let i=0; i<nodeNames.length; i++) {
+    nameToIndex[nodeNames[i]] = i
+    indexToName[i] = nodeNames[i]
+  }
 
-  svg = svg.call(d3.zoom().on("zoom", zoomed)).append("g");
+  const rowOfZeroes = []
+  for (let i=0; i<nodeNames.length; i++) {
+    rowOfZeroes.push(0)
+  }
+  const xChildByYParentMatrix = []
+  for (let i=0; i<nodeNames.length; i++) {
+    xChildByYParentMatrix.push([].concat(rowOfZeroes))
+  }
+  zipkinDependencyResponse.map(parentChildRow => {
+    xChildByYParentMatrix[nameToIndex[parentChildRow.child]][nameToIndex[parentChildRow.parent]] = parentChildRow.callCount
+  })
+  return {
+    nameToIndex: nameToIndex,
+    indexToName: indexToName,
+    matrix: xChildByYParentMatrix
+  }
+}
 
-  svg.append("defs").append("marker")
-    .attr("id", "arrow")
-    .attr("viewBox", "0 -5 10 10")
-    .attr("refX", 20)
-    .attr("refY", 0)
-    .attr("markerWidth", 8)
-    .attr("markerHeight", 8)
-    .attr("orient", "auto")
-    .append("svg:path")
-    .attr("d", "M0,-5L10,0L0,5");
-
-  var color = d3.scaleOrdinal(d3.schemeCategory10);
-
-  var simulation = d3.forceSimulation()
-    .force("link", d3.forceLink().id(function(d) { return d.id; }))
-    .force("charge", d3.forceManyBody().strength(-1000).distanceMax(400).distanceMin(10))
-    .force("center", d3.forceCenter(width / 2, height / 2));
-
-  var link = svg.append("g")
-    .attr("class", "links")
-    .selectAll("line")
-    .data(d3GraphData.links)
-    .enter().append("line")
-    .attr("stroke", function(d) { return color(d.type); })
-    .attr("marker-end", "url(#arrow)");
-
-
-  var node = svg.append("g")
-    .attr("class", "nodes")
-    .selectAll("circle")
-    .data(d3GraphData.nodes)
-    .enter().append("circle")
-    .attr("r", 10)
-    .attr("fill", function(d) { if (d.root == "true") return color(d.root); return color(d.type); })
-    .call(d3.drag()
-      .on("start", dragstarted)
-      .on("drag", dragged)
-      .on("end", dragended));
-
-  var text = svg.append("g").attr("class", "labels").selectAll("g")
-    .data(d3GraphData.nodes)
+function buildVisualization(d3MatrixData) {
+  var matrix = [
+    [11975,  5871, 8916, 2868],
+    [ 1951, 10048, 2060, 6171],
+    [ 8010, 16145, 8090, 8045],
+    [ 1013,   990,  940, 6907]
+  ];
+  var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height"),
+    outerRadius = Math.min(width, height) * 0.5 - 200,
+    innerRadius = outerRadius - 30;
+  var formatValue = d3.formatPrefix(",.0", 1e3);
+  var chord = d3.chord()
+    .padAngle(0.05)
+    .sortSubgroups(d3.descending);
+  var arc = d3.arc()
+    .innerRadius(innerRadius)
+    .outerRadius(outerRadius);
+  var ribbon = d3.ribbon()
+    .radius(innerRadius);
+  var color = d3.scaleOrdinal()
+    .domain(d3.range(4))
+    .range(["#000000", "#FFDD89", "#957244", "#F26223"]);
+  var g = svg.append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+    .datum(chord(d3MatrixData.matrix));
+  var group = g.append("g")
+    .attr("class", "groups")
+    .selectAll("g")
+    .data(function(chords) {
+      // console.log(chords)
+      return chords.groups; })
     .enter().append("g");
+  group.append("path")
+    .style("fill", function(d) { return color(d.index); })
+    .style("stroke", function(d) { return d3.rgb(color(d.index)).darker(); })
+    .attr("d", arc);
+  // var groupTick = group.selectAll(".group-tick")
+  //   .data(function(d) {
+  //     // console.log(d)
+  //     // return groupTicks(d, 1e3);
+  //     return d
+  //   })
+  //   .enter().append("g")
+  //   .attr("class", "group-tick")
+  //   .attr("transform", function(d) { return "rotate(" + (d.angle * 180 / Math.PI - 90) + ") translate(" + outerRadius + ",0)"; });
+  // groupTick.append("line")
+  //   .attr("x2", 6);
+  // groupTick
+  //   // .filter(function(d) { return d.value % 5e3 === 0; })
+  //   .data(function(d) {
+  //     console.log(d)
+  //     return d
+  //   })
+  //   .append("text")
+  //   .attr("x", 8)
+  //   .attr("dy", ".35em")
+  //   .attr("transform", function(d) { return d.angle > Math.PI ? "rotate(180) translate(-16)" : null; })
+  //   .style("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
+  //   .text(function(d) {
+  //     console.log(d)
+  //     // return formatValue(d.value);
+  //     return d3MatrixData.indexToName[d.value];
+  //   });
+  g.append("g")
+    .attr("class", "ribbons")
+    .selectAll("path")
+    .data(function(chords) { return chords; })
+    .enter().append("path")
+    .attr("d", ribbon)
+    .style("fill", function(d) { return color(d.target.index); })
+    .style("stroke", function(d) { return d3.rgb(color(d.target.index)).darker(); });
 
-  text.append("text")
-    .attr("x", 14)
-    .attr("y", ".31em")
-    .style("font-family", "sans-serif")
-    .style("font-size", "0.7em")
-    .text(function(d) { return d.id; });
 
-  node.on("click",function(d){
-    console.log("clicked", d.id);
-  });
-
-
-  node.append("title")
-    .text(function(d) { return d.id; });
-
-  simulation
-    .nodes(d3GraphData.nodes)
-    .on("tick", ticked);
-
-  simulation.force("link")
-    .links(d3GraphData.links);
-
-
-  function ticked() {
-    link
-      .attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
-
-    node
-      .attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; });
-
-    text
-      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-
-
-  }
-
-
-  function dragstarted(d) {
-    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-    d.fx = d.x;
-    d.fy = d.y;
-  }
-
-  function dragged(d) {
-    d.fx = d3.event.x;
-    d.fy = d3.event.y;
-  }
-
-  function dragended(d) {
-    if (!d3.event.active) simulation.alphaTarget(0);
-    d.fx = null;
-    d.fy = null;
-  }
-
-  function zoomed() {
-    svg.attr("transform", "translate(" + d3.event.transform.x + "," + d3.event.transform.y + ")" + " scale(" + d3.event.transform.k + ")");
-  }
+  //Append the label names on the outside
+  group.append("text")
+    .each(function(d) {console.log(d); d.angle = (d.startAngle + d.endAngle) / 2; })
+    .attr("dy", ".35em")
+    .attr("class", "titles")
+    .attr("text-anchor", function(d) {console.log(d); return d.angle > Math.PI ? "end" : null; })
+    .attr("transform", function(d) {
+      console.log(d, d.angle, (d.angle * 180 / Math.PI - 90))
+      return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
+        + "translate(" + (outerRadius + 10) + ")"
+        + (d.angle > Math.PI ? "rotate(180)" : "");
+    })
+    .text(function(d,i) { return d3MatrixData.indexToName[i]; });
+// Returns an array of tick angles and values for a given group and step.
+//   function groupTicks(d, step) {
+//     // console.log(d, step)
+//     var k = (d.endAngle - d.startAngle) / d.value;
+//     return d3.range(0, d.value, step).map(function(value) {
+//       return {index: d.index, value: value, angle: value * k + d.startAngle};
+//     });
+//   }
 }
 
 fetch(`${zipkinBaseUrl}/api/v1/dependencies?endTs=1525428438433&lookback=86399999`, {
@@ -148,7 +164,8 @@ fetch(`${zipkinBaseUrl}/api/v1/dependencies?endTs=1525428438433&lookback=8639999
 }).then(function(response) {
   response.json().then((obj) => {
     console.log(zipkinDepsToD3Graph(obj))
-    buildVisualization(zipkinDepsToD3Graph(obj))
+    console.log(zipkinDepsToD3Matrix(obj))
+    buildVisualization(zipkinDepsToD3Matrix(obj))
   })
 }).catch(function(err) {
   console.log(err)
