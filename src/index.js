f407637a30dea87184d4da58c8cddc6bef255398
Steve Conover <steve.conover@soundcloud.com>
2018-05-04 14:00:55 +0200

    do a zipking dependency lookup. specify ZIPKIN_BASE_URL as an env var to watchify.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 const d3 = require("d3")
 
-console.log("hello world 6")
+const zipkinBaseUrl = process.env.ZIPKIN_BASE_URL
 
+console.log("hello world 6", zipkinBaseUrl)
+
+fetch(`${zipkinBaseUrl}/api/v1/dependencies?endTs=1525428438433&lookback=86399999`, {
+  method: 'get'
+}).then(function(response) {
+  console.log(response)
+}).catch(function(err) {
+  console.log(err)
+});
